9ee0fae5d497c6b2f1b708b9b563a48d3e3319f4
joynabaminadib <joynabaminadib@gmail.com>
2019-01-07 16:33:57 -0500

    Joynab

--- Amex/src/main/java/search/SearchPage.java
+++ Amex/src/main/java/search/SearchPage.java
@@ -1,8 +1,13 @@
 package search;
 
 import base.CommonApi;
+import org.openqa.selenium.support.PageFactory;
 
 public class SearchPage extends CommonApi {
+public SearchPage(){
+    PageFactory.initElements(driver,this);
+}
+
     public void searchMenu(){
 
 clickByCss("#label-business");
--- Amex/src/test/java/searchTest/SearchTest.java
+++ Amex/src/test/java/searchTest/SearchTest.java
@@ -1,10 +1,17 @@
 package searchTest;
 
+//import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import search.SearchPage;
 
 public class SearchTest extends SearchPage {
-    @Test
+   SearchPage searchPage;
+   @BeforeMethod
+    public void setUp(){
+        this.searchPage = new SearchPage();
+    }
+@Test
    public void test1(){
         searchMenu();
     }
