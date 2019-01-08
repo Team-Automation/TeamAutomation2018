a40d99eb75428a9ee06b50b50025e2c3862d0d7c
AhmedFoysolHasan <45337687+AhmedFoysolHasan@users.noreply.github.com>
2019-01-07 23:14:50 -0500

    Add files via upload

---
+++ Target/Target/src/main/java/Target.java
@@ -0,0 +1,42 @@
+import base.CommonApi;
+import org.openqa.selenium.By;
+
+public class Target extends CommonApi{
+
+    public void search(){
+        typeOnId("search","Iphone Xs Max");
+        clickByXpath("//*[@type='submit']");
+    }
+
+    public void signIn()  {
+        clickByCss("#account");
+        clickByXpath("//*[@id='accountNav-signIn']/a/div");
+        typeOnCss("#username","AhmedFoysolHasan");
+        typeOnCss("#password","Abcd1234");
+        clickByXpath("//button[@id='login']");
+        System.out.println(driver.findElement(By.cssSelector("#username--longInstruction")).getText());
+    }
+
+    public void createAccount()  {
+        clickByCss("#account");
+        clickByXpath("//li[@id='accountNav-createAccount']//div[@class='Row-uds8za-0 kjjHUt']");
+        typeOnCss("#username","AhmedFoysolHasan");
+        typeOnCss("#firstname","Ahmed");
+        typeOnCss("#lastname","Hasan");
+        typeOnCss("#phone","9292477058");
+        typeOnCss("#password","Abcd1234");
+        System.out.println(driver.findElement(By.cssSelector("#username--longInstruction")).getText());
+    }
+
+    public void giftCard(){
+        clickByLinkText("Gift Cards");
+        clickByXpath("//*[@id='mainContainer']/div[3]/div/div[2]/div/div[1]/div[1]/div/div/a/div[2]");
+        typeOnCss("#giftCardNumber","123-456-789-123-457");
+        typeOnCss("#accessNumber","12345678");
+        clickByCss("#queryGiftCard");
+        System.out.println(driver.findElement(By.cssSelector(".styles__ToastContent-h3myfk-1.hWreYc")).getText());
+    }
+
+
+
+}
---
+++ Target/Target/src/test/java/TargetTest.java
@@ -0,0 +1,30 @@
+import org.testng.annotations.Test;
+
+public class TargetTest extends Target {
+
+    @Test
+    public void test1(){
+
+        search();
+    }
+
+    @Test
+    public void test2 (){
+        signIn();
+    }
+
+
+    @Test
+    public void test3 () { createAccount();
+    }
+
+    @Test
+    public void test4 () {
+       giftCard();
+    }
+
+
+
+
+
+}
