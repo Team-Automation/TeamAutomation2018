1984885f300f562fae3f1b3a9befbed06719ca28
AhmedFoysolHasan <45337687+AhmedFoysolHasan@users.noreply.github.com>
2019-01-11 15:47:29 -0500

    Add files via upload

--- Target/src/main/java/target/SearchPage.java
+++ Target/src/main/java/target/SearchPage.java
@@ -9,34 +9,6 @@ import org.openqa.selenium.support.PageFactory;
 
 public class SearchPage {
 
-        @FindBy(css = "#search")
-        WebElement searchInputWebElement;
-
-        @FindBy(css = "[type='submit']")
-        WebElement submitButtonWebElement;
-
-        public SearchPage (WebDriver driver) {
-            PageFactory.initElements(driver,this);
-        }
-
-        public WebElement getSearchInputWebElement() {
-            return searchInputWebElement;
-        }
-
-        public WebElement getSubmitButtonWebElement() {
-            return submitButtonWebElement;
-        }
-        public void searchFor(String value){
-            getSearchInputWebElement().sendKeys(value);
-        }
-        public void submitSearchButton(){
-            getSubmitButtonWebElement().click();
-        }
-
-        public void searchInputandSubmitButton () {
-            searchFor("Iphone xs");
-            submitSearchButton();
-        }
-
+      
 
 }
