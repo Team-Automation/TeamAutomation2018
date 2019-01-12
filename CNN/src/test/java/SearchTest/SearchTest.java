93a326c3864f09194b3eda26a159cbaee55fa90f
Hasib Ahmed <45337938+hasibahmed92@users.noreply.github.com>
2019-01-11 22:38:39 -0500

    Add files via upload

--- CNN/src/test/java/SearchTest/SearchTest.java
+++ CNN/src/test/java/SearchTest/SearchTest.java
@@ -74,26 +74,26 @@ public class SearchTest extends SearchPage {
         searchPage.getEmail().sendKeys("Ahmed", Keys.ENTER);
         searchPage.getPassword().sendKeys("fewggfguqgw",Keys.ENTER);
     }
-//    @Test
-//    public void LogInTest(){logIn();}
-//    @Test
-//    public void test1() { liveTv(); }
-//    @Test
-//    public void test2() { travelP(); }
-//    @Test
-//    public void test3(){ worldAfrica(); }
-//    @Test
-//    public void test4(){ LatestPolitics(); }
-//    @Test
-//    public void test5(){facebook(); }
-//    @Test
-//    public void test6(){UScrime();}
-//    @Test
-//    public void test7(){TechBusiness();}
-//    @Test
-//    public void test8 () {SocialOpinion();}
-//    @Test
-//    public void test9 (){HealthFitness();}
-//    @Test
-//    public void test10 (){ starsEntertainment();}
+    @Test
+    public void LogInTest(){logIn();}
+    @Test
+    public void test1() { liveTv(); }
+    @Test
+    public void test2() { travelP(); }
+    @Test
+    public void test3(){ worldAfrica(); }
+    @Test
+    public void test4(){ LatestPolitics(); }
+    @Test
+    public void test5(){facebook(); }
+    @Test
+    public void test6(){UScrime();}
+    @Test
+    public void test7(){TechBusiness();}
+    @Test
+    public void test8 () {SocialOpinion();}
+    @Test
+    public void test9 (){HealthFitness();}
+    @Test
+    public void test10 (){ starsEntertainment();}
 }
