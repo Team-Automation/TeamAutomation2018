eccdd5267a3aaa27e39dc27bf6dd836f6aa56d4f
Lazimul <45337813+Lazimul@users.noreply.github.com>
2019-01-12 09:46:16 -0500

    test case added

---
+++ Ebay/src/main/java/myApi/EbayApi.java
@@ -0,0 +1,104 @@
+package myApi;
+
+import base.CommonApi;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+public class EbayApi extends CommonApi {
+    public void typeOnElement(String locator, String value) {
+        try {driver.findElement(By.cssSelector(locator)).sendKeys(value);
+        } catch (Exception ex1) {
+            try {
+                System.out.println("First Attempt was not successful");
+                driver.findElement(By.name(locator)).sendKeys(value);
+            } catch (Exception ex2) {
+                try {
+                    System.out.println("Second Attempt was not successful");
+                    driver.findElement(By.xpath(locator)).sendKeys(value);
+                } catch (Exception ex3) {
+                    System.out.println("Third Attempt was not successful");
+                    driver.findElement(By.id(locator)).sendKeys(value);
+                }
+            }
+        }
+    }
+
+    public void typeOnElementEntry(String locator, String value) {
+        try {
+            driver.findElement(By.cssSelector(locator)).sendKeys(value, Keys.ENTER);
+        } catch (Exception ex1) {
+            try {
+                System.out.println("First Attempt was not successful");
+                driver.findElement(By.name(locator)).sendKeys(value, Keys.ENTER);
+            } catch (Exception ex2) {
+                try {
+                    System.out.println("Second Attempt was not successful");
+                    driver.findElement(By.xpath(locator)).sendKeys(value, Keys.ENTER);
+                } catch (Exception ex3) {
+                    System.out.println("Third Attempt was not successful");
+                    driver.findElement(By.id(locator)).sendKeys(value, Keys.ENTER);
+                }
+            }
+        }
+    }
+
+    public void typeOnElementNEnterCss(String locator, String value){
+        driver.findElement(By.cssSelector(locator)).sendKeys(value, Keys.ENTER);
+    }
+    public void typeOnElementNEnterClass (String locator, String value){
+
+        driver.findElement(By.className(locator)).sendKeys(value, Keys.ENTER);
+    }
+
+    public void getEelementsByCss(String locator){
+        driver.findElement(By.cssSelector(locator)).click();
+    }
+    public void getSubmitByXpath(String locator){
+        driver.findElement(By.xpath(locator)).submit();
+    }
+    public void getEelementsByxpath(String locator){
+        driver.findElement(By.xpath(locator)).click();
+    }
+    public void getEelementsByID(String locator){
+        driver.findElement(By.id(locator)).click();
+    }
+
+    public void clearField(String locator){
+        driver.findElement(By.id(locator)).clear();
+    }
+    public void selectOnid1(String locator){
+        Select select1 = new Select(driver.findElement(By.id(locator)));
+
+    }
+    public void putElementOnCss(String locator, String value){
+        driver.findElement(By.cssSelector(locator)).sendKeys(value);
+    }
+    public void selectElementOnxpath(String locator){
+        WebElement select = driver.findElement(By.xpath(locator));select.click();
+
+    }
+    public void selectElementOnclass (String locator){
+        WebElement select4 = driver.findElement(By.className(locator));select4.click();
+    }
+
+    public void selectOnid2(String locator, String value) {
+        Select select2 = new Select(driver.findElement(By.id(locator)));
+        select2.selectByVisibleText(value);
+    }
+    public void selectOnname(String locator, String value) {
+        Select select3 = new Select(driver.findElement(By.name(locator)));
+        select3.selectByVisibleText(value);
+    }
+    public String username= "haquelazimul@yahoo.com";
+    public String password= "1993";
+    public String typename= "Lazimul";
+    public String typelName= "haque";
+    public String cpassword="710727";
+    public String searchproduct="polo";
+
+
+
+
+}
---
+++ Ebay/src/test/java/TestHome.java
@@ -0,0 +1,12 @@
+import myApi.search.SearchPage;
+import org.testng.annotations.Test;
+
+public class TestHome extends SearchPage {
+    @Test
+    public void test1(){
+        register();
+        SearchOption();
+        login();
+
+    }
+}
