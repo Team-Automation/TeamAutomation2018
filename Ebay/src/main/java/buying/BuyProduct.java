28bdc319e5596e157c9ccb7e520c73aa9fa9a271
Lazimul <45337813+Lazimul@users.noreply.github.com>
2019-01-12 01:48:10 -0500

    test case added

--- Ebay/src/main/java/buying/BuyProduct.java
+++ Ebay/src/main/java/buying/BuyProduct.java
@@ -6,8 +6,23 @@ import org.openqa.selenium.support.FindBy;
 
 public class BuyProduct {
     public class buy extends EbayApi{
+        @FindBy (css = "#gh-ac")
+        WebElement searchproduct;
+        @FindBy (id = "gh-btn")
+        WebElement clickproduct;
         @FindBy (xpath = "//*[@id=\"srp-river-results-listing8\"]/div/div[1]/div/a/div/img")
         WebElement product1;
+
+        public void dosearch(String Value){
+            searchproduct.sendKeys(Value);
+        }
+        public void clickit(){
+            clickproduct.click();
+        }
+        public void clickpolo(){
+            product1.click();
+        }
+
 
     }
 }
--- Ebay/src/test/java/Test/Buyingstuff.java
+++ Ebay/src/test/java/Test/Buyingstuff.java
@@ -4,13 +4,18 @@ import buying.BuyProduct;
 import myApi.EbayApi;
 import myApi.login.CreateLogSearch;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 public class Buyingstuff  extends EbayApi {
-    CreateLogSearch createLogSearch;
-    BuyProduct Buy;
+   // CreateLogSearch createLogSearch;
+    BuyProduct buy;
 
     public void initialize1() {
-        createLogSearch = PageFactory.initElements(driver, CreateLogSearch.class);
+        //createLogSearch = PageFactory.initElements(driver, CreateLogSearch.class);
+        buy=PageFactory.initElements(driver,BuyProduct.class);
+    }
+    @Test
+    public void startSearch(){
 
     }
 }
