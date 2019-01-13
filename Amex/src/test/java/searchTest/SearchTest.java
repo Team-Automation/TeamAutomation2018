2da464e08a7d171aa6b9ca70c980bb384a5e8d22
joynabaminadib <45337697+joynabaminadib@users.noreply.github.com>
2019-01-13 08:55:48 -0500

    update

--- Amex/src/test/java/searchTest/SearchTest.java
+++ Amex/src/test/java/searchTest/SearchTest.java
@@ -40,7 +40,63 @@ public class SearchTest extends SearchPage {
     public void test3() throws InterruptedException, Exception {
 
         String actual = searchPage.clicksearch3();
-        String expected = "https://global.americanexpress.com/search?term=APR";
+        String expected = "Where can I find my Annual Percentage Rate (APR) online?";
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test4() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch4();
+        String expected = "Can I use Membership Rewards® points to book travel on the American Express Travel site?";
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test5() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch5();
+        String expected = "American Express Serve® Cash Back, your prepaid debit account. The only prepaid rewards program with 1% Cash Back on purchases.";
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test6() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch6();
+        String expected = "Login to your American Express® reloadable prepaid card account to view your history, load your card, get frequently asked questions answered, and more.";
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test7() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch7();
+        String expected = null;
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test8() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch8();
+        String expected = "Accept Credit Cards | American Express®";
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test9() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch9();
+        String expected = "American Express Supplier Management and Supply Chain Inclusion";
+        Assert.assertEquals (actual,expected);
+
+    }
+    @Test
+    public void test10() throws InterruptedException, Exception {
+
+        String actual = searchPage.clicksearch3();
+        String expected = "Choose the Membership Rewards®";
         Assert.assertEquals (actual,expected);
 
     }
